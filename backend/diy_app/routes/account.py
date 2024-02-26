df35796bb243967da4fa36abe9e44ebebd42c743
August-IE <Aeromosele9@gmail.com>
2024-02-26 08:00:45 -0800

    implement user sign-in dialogue

--- backend/diy_app/routes/account.py
+++ backend/diy_app/routes/account.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import sys
 from diy_app.extensions import db
 from flask import request, jsonify
 from diy_app.models.user import User
@@ -19,3 +20,19 @@ def signup():
     db.session.commit()
 
     return jsonify({'message': 'User created successfully'}), 201
+
+# Endpoint to sign in a user
+@app_routes.route('/signin', methods=['POST'])
+def signin():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('pwd')
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and user.check_password(pwd):
+        # User is authenticated
+        return jsonify({'message': 'Sign-in successful'}), 200
+    else:
+        # Invalid credentials
+        return jsonify({'message': 'Invalid username or password'}), 401

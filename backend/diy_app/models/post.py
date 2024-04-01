c722b1706fbf20ebe3bf15a5857940266a7acdef
Bamiwo Adebayo <bammietop@gmail.com>
2024-04-01 19:23:41 +0000

    Deploying backend

--- backend/diy_app/models/post.py
+++ backend/diy_app/models/post.py
@@ -30,7 +30,7 @@ class Post(Base):
     
     def to_dict_with_images(self):
         images = json.loads(self.image_filenames)
-        image_urls = ['http://100.26.50.175/_uploads/photos/' + image for image in images]
+        image_urls = ['https://web-02.bammietop.tech/_uploads/photos/' + image for image in images]
         return {
             'id': self.id,
             'title': self.title,

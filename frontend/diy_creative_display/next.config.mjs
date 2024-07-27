a0b8917ddf6a762418a51499a81afa22b9996107
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-07-27 22:04:24 +0100

    Removed the middleware file

--- frontend/diy_creative_display/next.config.mjs
+++ frontend/diy_creative_display/next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
-// import middleware from "./src/middleware";
 
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "**",
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '**',
       },
     ],
   },
